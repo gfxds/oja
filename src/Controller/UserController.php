8bf2906853cc2d0128932d9d7884f9b47a1cef6a
dd <dan--smith@hotmail.co.uk>
2022-03-03 06:24:56 +0000

    json endpoints for user data

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,14 @@
 // src/Controller/UserController.php
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class UserController
@@ -23,5 +26,44 @@ class UserController extends AbstractController
     {
         return $this->render('user.html.twig');
     }
+  
+    /**
+     * List all users
+     * @Route("/users", name="show_users")
+     */
+    public function showUsers(ManagerRegistry $doctrine): JsonResponse
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $users = $repository
+            ->createQueryBuilder('p')
+            ->getQuery()
+            ->getArrayResult();
 
+        return new JsonResponse($users);
+    }
+
+    /**
+     * List user by id
+     * @Route("/users/{id}", name="show_user")
+     */
+    public function showUser(
+        SerializerInterface $serializer,
+        ManagerRegistry $doctrine,
+        int $id): JsonResponse
+    {
+        $user = $doctrine->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            return new JsonResponse(
+                [
+                    "code" => 404,
+                    "error" => "user not found"
+                ],404
+            );
+        }
+
+        return new JsonResponse(
+            json_decode($serializer->serialize($user, 'json'),true)
+        );
+    }
 }
